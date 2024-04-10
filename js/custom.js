f13937bd20213d53e2ca73d9c0c3853fd2c89010
ManoDev18 <166386236+ManoDev18@users.noreply.github.com>
2024-04-10 19:04:39 +0530

    Add files via upload
    Added postivius files

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+/*********accrodian*********/
+$(document).ready(function () {
+	$(".set > a").on("click", function () {
+		if ($(this).hasClass("active")) {
+            $(this).parent(".set").removeClass("active");
+			$(this).removeClass("active");
+			$(this)
+				.siblings(".content")
+				.slideUp(200);
+			$(".set > a i")
+				.removeClass("fa-minus")
+				.addClass("fa-plus");
+		} else {
+            $(this).parent(".set").addClass("active");
+			$(".set > a i")
+				.removeClass("fa-minus")
+				.addClass("fa-plus");
+			$(this)
+				.find("i")
+				.removeClass("fa-plus")
+				.addClass("fa-minus");
+			$(".set > a").removeClass("active");
+			$(this).addClass("active");
+			$(".content").slideUp(200);
+			$(this)
+				.siblings(".content")
+				.slideDown(200);
+		}
+	
+    });
+});
